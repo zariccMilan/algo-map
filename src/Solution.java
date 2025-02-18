5b7d7eec1da1acf6dd274c4a692607d9e1cf6958
Milan Zaric <zaric.milan11@gmail.com>
2025-02-18 22:52:20 +0100

    2239. Find Closest Number to Zero Solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,29 @@
+
 public class Solution {
-    public int findClosestNumber(int [] nums) { 
-        return -1;
+    public int findClosestNumber(int[] nums) { 
+        int closest = nums[0];
+
+        for (int x : nums) {
+            if (Math.abs(x) < Math.abs(closest)) {
+                closest = x;
+            }
+        }
+
+        if (closest < 0 && contains(nums, Math.abs(closest))) {
+            return Math.abs(closest);
+
+        } else {
+            return closest;
+        }
+    }
+
+    private boolean contains(int[] nums, int value) {
+        for (int num : nums) {
+            if (num == value) {
+                return true;
+            }
+        }
+        return false;
+
     }
 }
